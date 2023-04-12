48f23e33d65ab65a227c644988119861d06211f1
Thuan Pham <thuanowa@gmail.com>
2023-04-12 10:09:48 +0700

    refactor: clean code

--- scripts/switch_session_window.sh
+++ scripts/switch_session_window.sh
@@ -3,28 +3,26 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function main {
-  local sessions
-  local session
-  local query
-  local sess_arr
-  local retval
-  sessions=$(tmux list-windows -a | 
-    fzf --exit-0 --print-query --reverse)
-  retval=$?
+	local sessions
+	local session
+	local query
+	local sess_arr
+	local retval
+	sessions=$(tmux list-windows -a |
+		fzf --exit-0 --print-query --reverse)
+	retval=$?
 
-  IFS=$'\n' read -rd '' -a sess_arr <<<"$sessions"
+	IFS=$'\n' read -rd '' -a sess_arr <<<"$sessions"
 
-  session=$(echo ${sess_arr[1]} | sed 's/: .*//g')
-  query=${sess_arr[0]}
+	session=$(echo ${sess_arr[1]} | sed 's/: .*//g')
+	query=${sess_arr[0]}
 
-  if [ $retval == 0 ]; then
-    if [ "$session" == "" ]; then
-        session=$(echo "$query" | sed 's/: .*//g')
-    fi
-    tmux switch-client -t "$session"
-  elif [ $retval == 1 ]; then
-    tmux command-prompt -b -p "Press enter to create and go to [$query] session" \
-      "run '$CURRENT_DIR/make_new_session.sh \"$query\" \"%1\"'"
-  fi
+	if [ $retval == 0 ] && [ "$session" == "" ]; then
+		session=$(echo "$query" | sed 's/: .*//g')
+		tmux switch-client -t "$session"
+	elif [ $retval == 1 ]; then
+		tmux command-prompt -b -p "Press enter to create and go to [$query] session" \
+			"run '$CURRENT_DIR/make_new_session.sh \"$query\" \"%1\"'"
+	fi
 }
 main
